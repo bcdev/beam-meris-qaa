805d762002ea1dbd0069d344b2a1df01f31d5c43
tom <tom.block@brockmann-consult.de>
2013-02-20 17:19:22 +0100

    intermediate

---
+++ src/main/java/org/esa/beam/meris/qaa/BinProcessor.java
@@ -0,0 +1,15 @@
+package org.esa.beam.meris.qaa;
+
+import org.esa.beam.binning.Bin;
+
+
+public abstract class BinProcessor {
+
+    abstract float[] process(Bin bin);
+
+    //  @todo create Processor tb 2013-02-20
+    //  @todo description tb 2013-02-20
+    //  @todo description of input and output parameter tb 2013-02-20
+
+    // have a look at BEAM binning2
+}
---
+++ src/test/java/org/esa/beam/meris/qaa/ImaginaryNumberExceptionTest.java
@@ -0,0 +1,16 @@
+package org.esa.beam.meris.qaa;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class ImaginaryNumberExceptionTest {
+
+    @Test
+    public void testConstructAndGetMessage() {
+        final ImaginaryNumberException exception = new ImaginaryNumberException("oopsi", -98);
+
+        assertEquals("oopsi: -98.0", exception.getMessage());
+    }
+}
