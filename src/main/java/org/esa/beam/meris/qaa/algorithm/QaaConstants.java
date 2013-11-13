35aa8f7ed59884b7923c8802f2dd949dbaa32ef9
marpet <marco.peters@brockmann-consult.de>
2013-11-13 18:03:29 +0100

    release 1.3.1

--- src/main/java/org/esa/beam/meris/qaa/algorithm/QaaConstants.java
+++ src/main/java/org/esa/beam/meris/qaa/algorithm/QaaConstants.java
@@ -27,11 +27,11 @@ public final class QaaConstants {
 
 
     // aw and bbw coefficients from IOP datafile
-    static final double[] AW_COEFS = {
+    public static final double[] AW_COEFS = {
             0.00469, 0.00721, 0.015,
             0.0325, 0.0619, 0.2755
     };
-    static final double[] BBW_COEFS = {
+    public static final double[] BBW_COEFS = {
             0.003328, 0.0023885, 0.001549,
             0.0012992, 0.0008994, 0.0005996
     };
