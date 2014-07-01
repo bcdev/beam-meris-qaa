a3c17d541f1fab4e0917a9cd8c712e73d2d1e652
marpet <marco.peters@brockmann-consult.de>
2014-07-01 16:41:26 +0200

    some small adaptations of the master

--- src/main/resources/doc/help/qaa.hs
+++ src/main/resources/doc/help/qaa.hs
@@ -4,7 +4,7 @@
         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
-    <title>QAA_v4 Processor Help</title>
+    <title>QAA Processor Help</title>
     <maps>
         <homeID>top</homeID>
         <mapref location="map.jhm"/>
